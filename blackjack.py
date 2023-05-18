d50d2e9ef19e977093552601fd28423ca61f192c
Rajan Ladwa <rladwa@gmail.com>
2023-05-18 15:16:07 +0900

    add classes

--- blackjack.py
+++ blackjack.py
@@ -6,3 +6,62 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
         'Queen':10, 'King':10, 'Ace':11}
 
 playing = True
+
+class Card:
+    
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+
+class Deck:
+    
+    def __init__(self):
+        self.deck = []  # start with an empty list
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
+    
+    def __str__(self):
+        deck_cards = ''
+        for card in self.deck:
+            deck_cards += '\n' + card.__str__()
+        return 'The deck has: ' + deck_cards
+        
+    def shuffle(self):
+        random.shuffle(self.deck)
+        
+    def deal(self):
+        draw_card = self.deck.pop()
+        return draw_card
+
+class Hand:
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+    
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+        if card.rank == 'Ace':
+                self.aces += 1
+    
+    def ace_count(self):
+        while self.value > 21 and self.aces > 0:
+            self.value -= 10
+            self.aces -= 1
+
+class Chips:
+    
+    def __init__(self):
+        self.total = 100  # This can be set to a default value or supplied by a user input
+        self.bet = 0
+        
+    def win_bet(self):
+        self.total += self.bet
+    
+    def lose_bet(self):
+        self.total -= self.bet
